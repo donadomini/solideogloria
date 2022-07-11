8f430ab04b87e59261cf980dc5d7a15d68d259cb
donadomini <66259546+donadomini@users.noreply.github.com>
2022-07-11 22:27:03 +0900

    Add files via upload

--- hidden.js
+++ hidden.js
@@ -2,18 +2,10 @@ const el = document.getElementById('box');
 
 const hiddenEl = document.getElementById('hidden');
 
-// ✅ Show hidden DIV on hover
 el.addEventListener('mouseover', function handleMouseOver() {
   hiddenEl.style.display = 'block';
-
-  // 👇️ if you used visibility property to hide div
-  // hiddenEl.style.visibility = 'visible';
 });
 
-// ✅ (optionally) Hide element on mouse out
 el.addEventListener('mouseout', function handleMouseOut() {
   hiddenEl.style.display = 'none';
-
-  // 👇️ if you used visibility property to hide div
-  // hiddenEl.style.visibility = 'hidden';
 });
